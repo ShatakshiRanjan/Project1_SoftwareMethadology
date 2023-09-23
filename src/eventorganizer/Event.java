551c8ee1b03318246d7c666726108f28763f4644
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2023-09-22 23:27:31 -0400

    Event

--- src/eventorganizer/Event.java
+++ src/eventorganizer/Event.java
@@ -1,4 +1,30 @@
 package eventorganizer;
 
-public class Event {
+public class Event implements Comparable<Event>{
+    private Date date;
+    private Timeslot startTime;
+    private Location location;
+    private Contact contact;
+    private int duration;
+
+    public static void main(String[] args){
+
+    }
+
+    public Event(Date date, Timeslot startTime, Location location, Contact contact, int duration) {
+        this.date = date;
+        this.startTime = startTime;
+        this.location = location;
+        this.contact = contact;
+        this.duration = duration;
+    }
+
+    public String toString(){
+        return "[Event Date: " + dateToString(date)
+    }
+
+    @Override
+    public int compareTo(Event o) {
+        return 0;
+    }
 }
